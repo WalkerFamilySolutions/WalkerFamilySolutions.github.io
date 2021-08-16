79d96ce70198bde0f0946e9802dc64d026b85ff2
nicolehouston <nicole.walker@leadcrunch.ai>
2021-08-16 10:53:55 -0500

    styling

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 export default function AboutUs() {
     return (
-        <div className='neonWrapper'>
-            <div style={{padding:'40px'}}>
-                <h2 className='welcome neonText'>  Welcome to Walker Family solutions! Have a look around! </h2>
-                <h3 className='aboutUs neonText'>About Us</h3>
-                <p className='bio neonText'>Austin and Nicole are a married devevlopment duo. Both bootcamp graduates, they spend their days vitrually zooming into work while standing side by side. With a combined 5 years of react experiance and 3 years of backend development are ready and willing to partner with your team to provide the best solution to yor development needs</p>
+        <div>
+            <div className="aboutUs">
+                <p>Welcome! We are a married development duo. Both coding bootcamp graduates, we spend our days working side by side, Austin as a coding instructor and Nicole as a software developer for a startup. With a combined 5 years of development experience we are ready and willing to partner with your team to provide the best solution for your development needs.</p>
             </div>
         </div>
     )
